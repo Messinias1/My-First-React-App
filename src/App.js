9f513586dd3fbabc4e982c7aef6b44fce842d098
Carl Kakisis <Messinias1@gmail.com>
2019-05-30 00:27:17 -0400

    Add Ninja file created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
-// import React from 'react';
-// import Ninjas from './Ninjas'
-
-// function App() {
-//   state = {
-//     ninjas : [
-//       {name: "Ryu", age: 30, belt: 'black', id: 1},
-//       {name: "Yoshi", age: 20, belt: 'green', id: 2},
-//       {name: "Crystal", age: 25, belt: 'pink', id: 3}
-//     ]
-//   }
-//   return (
-//     <div className="App">
-//       <h1>My First React App!</h1>
-//       <Ninjas ninjas={this.state.ninjas}/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import React, { Component } from 'react';
 import Ninjas from './Ninjas'
+import AddNinja from './AddNinja'
 
 class App extends Component {
   state = {
@@ -32,11 +10,19 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random()
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app</h1>
         <Ninjas ninjas={this.state.ninjas}/>
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
